0e505b241add35497c01b12bbdac6072d03590c5
Diana <di.tsevan@gmail.com>
2019-05-16 12:16:41 +0300

    flights page initial structure

---
+++ src/components/flights/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class FlightsPage extends Component {
+  render() {
+    return (
+      <section className="flights-page">
+        
+      </section>
+    )
+  }
+}
+
+export default FlightsPage;
